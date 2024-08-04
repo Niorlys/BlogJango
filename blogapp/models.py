126458b8527c711b4e0d3b42342bc6e2f9b4113d
Niorlys <niorlysernestoc@gmail.com>
2024-08-04 17:18:18 -0400

    Added tag and blogentry models

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Tags(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class BlogEntry(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    tags = models.ManyToManyField(Tags)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ["-published_at"]
+        indexes = [
+            models.Index(fields=["-publish_at"]),
+        ]
+
+    def __str__(self) -> str:
+        return self.title
